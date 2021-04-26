3603e6e7a3130cea981ffcf3f4067b541f900858
ParmarManpreet <manpreet.parmar29@hotmail.com>
2021-04-26 00:08:19 -0400

    Add Boostrap pt2

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup as bs4
 import requests
 from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 import requests
 import os
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route('/')
@@ -12,19 +14,18 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/analyse")
-def analyse():
-    prices=[]
-    url = request.args.get("url")
-    newurl = requests.get(url)
-    soup = bs4(newurl.text, 'html.parser')
+#@app.route("/analyse")
+#def analyse():
+    #prices=[]
+    #url = request.args.get("url")
+    #newurl = requests.get(url)
+    #soup = bs4(newurl.text, 'html.parser')
 
-    for tags in soup.findAll(attrs={"class" : "d-text d-fontSize--larger"}):
-        #soup = soup.findAll(attrs={"class" : "d-text d-fontSize--larger"}).getText()
-        price = tags.getText()
-        prices.append(price)
+    #for tags in soup.findAll(attrs={"class" : "d-text d-fontSize--larger"}):
+        #price = tags.getText()
+        #prices.append(price)
     # print(soup1)
-    return render_template("analyse.html", name=prices)
+    #return render_template("analyse.html", name=prices)
 
 
 if __name__ == '__main__':
