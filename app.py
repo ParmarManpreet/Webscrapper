2002955a615b9ff9ad0f4c860ab356a4e6f7bfc6
ParmarManpreet <manpreet.parmar29@hotmail.com>
2021-05-12 21:42:05 -0400

    able to update sheets with user inputs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs4
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from googleAPI import sheetsUpd
 import gspread
 import requests
 import os
@@ -16,20 +17,41 @@ def index():
 
 @app.route("/analyse", methods=['POST'])
 def analyse():
+    if request.method == 'POST':
+        price = request.form["prop_price"]
+        down = request.form["prop_down"]
+        rent = request.form["prop_rent"]
+        expense = request.form["prop_expense"]
+        tax = request.form["prop_tax"]
+        interest = request.form["prop_interest"]
 
-    return render_template("analyse.html")
-    #prices=[]
-    #url = request.args.get("url")
-    #newurl = requests.get(url)
-    #soup = bs4(newurl.text, 'html.parser')
+        sheetsUpd(price, down, rent, expense, tax, interest)
 
-    #for tags in soup.findAll(attrs={"class" : "d-text d-fontSize--larger"}):
-        #price = tags.getText()
-        #prices.append(price)
-    # print(soup1)
-    #return render_template("analyse.html", name=prices)
+        return render_template("analyse.html")
+
+        #spreadsheet_id = '1ag4C3fVMZRnoBeE3maBOB1nygAbNWF3uQYyi5P43l5M'
+        #gc = gspread.service_account(filename='credentials.json')
+        #sh = gc.open_by_key(spreadsheet_id)
+        #analysis_worksh = sh.sheet1
+
+        #analysis_worksh.update_cell(2,3, "test2")
+        #price = request.form["prop_price"]
+        #analysis_worksh.update_cell(2, 3, price)
+        #return render_template("analyse.html")
+
+
+        #prices=[]
+        #url = request.args.get("url")
+        #newurl = requests.get(url)
+        #soup = bs4(newurl.text, 'html.parser')
+
+        #for tags in soup.findAll(attrs={"class" : "d-text d-fontSize--larger"}):
+            #price = tags.getText()
+            #prices.append(price)
+        # print(soup1)
+        #return render_template("analyse.html", name=prices)
 
 
 if __name__ == '__main__':
-    app.debug=True
+    app.debug = True
     app.run()
--- googleAPI.py
+++ googleAPI.py
@@ -1,8 +1,27 @@
 import gspread
+from flask import Flask, render_template, request
 
-gc = gspread.service_account(filename='credentials.json')
-sh = gc.open_by_key('1ag4C3fVMZRnoBeE3maBOB1nygAbNWF3uQYyi5P43l5M')
-worksheet = sh.sheet1
+def sheetsUpd(price, down, rent, expense, tax, interest):
+    #Call sheets API
+    SPREADSHEET_ID = '1ag4C3fVMZRnoBeE3maBOB1nygAbNWF3uQYyi5P43l5M'
+    SECRET_FILE = 'credentials.json'
 
-res = worksheet.get_all_values()
-print(res)
+    gc = gspread.service_account(filename= SECRET_FILE)
+    sh = gc.open_by_key(SPREADSHEET_ID)
+    analysis_worksh = sh.sheet1
+
+    analysis_worksh.update_cell(4, 3, price)
+
+    #for down payment user needs to write percentage and I need to use it to calculate the downpayment
+    #analysis_worksh.update_cell(4, 3, down)
+    analysis_worksh.update_cell(13, 3, rent)
+
+    #for expense I need to allow user to write the type of expense and the amount and it show append in sheets
+    analysis_worksh.update_cell(8, 12, expense)
+
+    #for tax I need to give option to choose moncton or mtl and then for mtl they have 2 taxes to fill
+    analysis_worksh.update_cell(15, 3, tax)
+
+    #need to be able to put a decimal number
+    analysis_worksh.update_cell(8, 3, interest)
+    #return render_template("analyse.html")
